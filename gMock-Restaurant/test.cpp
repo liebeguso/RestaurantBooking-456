cf24590445c1c3a4c9d4c33fee2fe673c3b9f43d
seuni.park <seuni.park@samsung.com>
2025-07-24 12:55:13 +0900

    [feature] Step1 ~ Step3-2
    Signed-off-by: seuni.park <seuni.park@samsung.com>

--- gMock-Restaurant/test.cpp
+++ gMock-Restaurant/test.cpp
@@ -1,12 +1,47 @@
 #include "gmock/gmock.h"
 #include "booking_scheduler.cpp"
 
-TEST(BookingSchedulerTest, 예약은정시에만가능하다정시가아닌경우예약불가) {
+using namespace testing;
 
+class BookingFixture : public Test {
+public:
+	tm getTime(int year, int mon, int day, int hour, int min) {
+		tm result = { 0, min,hour, day, mon - 1, year - 1900, 0, 0, -1 };
+		mktime(&result);
+		return result;
+	}
+};
+
+TEST_F(BookingFixture, 예약은정시에만가능하다정시가아닌경우예약불가) {
+	// Arrange
+	tm notOntheHour = getTime(2021, 3, 26, 9, 5);
+
+	Customer customer{ "Fake name", "010-1234-5678" };
+	Schedule* schedule = new Schedule{ notOntheHour, 1, customer };
+	BookingScheduler bookingScheduler{ 3 };
+
+	// Act
+	EXPECT_THROW({
+		bookingScheduler.addSchedule(schedule);
+		}, std::runtime_error);
+
+	// Assert
+	// expected runtime exception
 }
 
-TEST(BookingSchedulerTest, 예약은정시에만가능하다정시인경우예약가능) {
+TEST_F(BookingFixture, 예약은정시에만가능하다정시인경우예약가능) {
+	// Arrange
+	tm OntheHour = getTime(2021, 3, 26, 9, 0);
 
+	Customer customer{ "Fake name", "010-1234-5678" };
+	Schedule* schedule = new Schedule{ OntheHour, 1, customer };
+	BookingScheduler bookingScheduler{ 3 };
+
+	// Act
+	bookingScheduler.addSchedule(schedule);
+
+	// Assert
+	EXPECT_EQ(true, bookingScheduler.hasSchedule(schedule));
 }
 
 TEST(BookingSchedulerTest, 시간대별인원제한이있다같은시간대에Capacity초과할경우예외발생) {
